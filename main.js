6d3a716a5fe0ace86d286eb7f12ec81225fdc8bb
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-15 14:01:08 +0600

    create multiple instance and use one instance property in another instance

--- main.js
+++ main.js
@@ -163,3 +163,22 @@ new Vue({
         name: "",
     },
 });
+ 
+const app11 = new Vue({
+    el: "#app1",
+    data: {
+        value: "1st",
+    },
+    methods: {
+        changeValue(){
+            app12.value="Changed";
+        },
+    },
+});
+
+const app12 = new Vue({
+    el: "#app2",
+    data: {
+        value: "2nd",
+    },
+});
