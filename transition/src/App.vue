1368c4a75bed3331621d483bdcd9daf609c9cf3a
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-22 21:29:54 +0600

    vue.js transition practice

---
+++ transition/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <transition name="appear">
+      <div class="p-3 mb-2 bg-success text-white" v-if="display">Hello</div>
+    </transition>
+    <button @click="display=!display" class="btn btn-primary">Toggle</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      display: false,
+    }
+  }
+}
+</script>
+
+<style>
+  .appear-enter{
+    opacity: 0;
+    transform: translateX(300px);
+  }
+  .appear-enter-active{
+    transition: all 2s ease;
+  }
+  .appear-leave{
+
+  }
+  .appear-leave-active{
+    opacity: 0;
+    transform: translateX(300px);
+    transition: all 2s ease;
+  }
+</style>
