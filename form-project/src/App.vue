35cbba81befc6fd2c05c8d7febe6b10bc6006bd4
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-22 15:52:46 +0600

    globally custom directive use

--- form-project/src/App.vue
+++ form-project/src/App.vue
@@ -1,33 +1,12 @@
 <template>
     <div class="container">
-        <button @click="compToRender='compHome'">Home</button>
-        <button @click="compToRender='compAbout'">About</button>
-        <button @click="compToRender='compContact'">Contact</button>
-
-        <keep-alive>
-            <component :is="compToRender"></component>
-        </keep-alive>
+        <div v-awesome.redColor.big="'Hello Shohel'"></div>
+        <div v-awesome.greenColor.small="'Hello Shohel'"></div>
     </div>
 </template>
 
 <script>
-import compHome from './Components/Home'
-import compAbout from './Components/About'
-import compContact from './Components/Contact'
 export default {
-    data(){
-        return{
-            compToRender: "compContact",
-        }
-    },
-    components:{
-        compHome,
-        compAbout,
-        compContact
-    }
+    
 }
 </script>
-
-<style>
-
-</style>
--- form-project/src/main.js
+++ form-project/src/main.js
@@ -3,6 +3,32 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('awesome',{
+  bind(el, binding){
+    el.innerHTML = binding.value;
+    // el.style.color= "red";
+    // if(binding.arg === 'redColor'){
+    //   el.style.color= "red";
+    // }
+    // if(binding.arg === 'greenColor'){
+    //   el.style.color= "green";
+    // }
+    if(binding.modifiers.redColor){
+      el.style.color = 'red';
+    }
+    if(binding.modifiers.big){
+      el.style.fontSize = '30px';
+    }
+
+    if(binding.modifiers.greenColor){
+      el.style.color = 'green';
+    }
+    if(binding.modifiers.small){
+      el.style.fontSize = '15px';
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
