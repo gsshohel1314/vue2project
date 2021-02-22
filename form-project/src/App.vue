7e86a02608ded3cdf0924163f702459b4e8a075a
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-22 20:58:59 +0600

    Locally custom directives use

--- form-project/src/App.vue
+++ form-project/src/App.vue
@@ -2,11 +2,24 @@
     <div class="container">
         <div v-awesome.redColor.big="'Hello Shohel'"></div>
         <div v-awesome.greenColor.small="'Hello Shohel'"></div>
+        <hr>
+        <div v-user="userValue"></div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            userValue: "Luffy",
+        }
+    },
+    directives:{
+        'user': {
+            bind(el, binding){
+                el.innerHTML = binding.value;
+            }
+        }
+    }
 }
 </script>
